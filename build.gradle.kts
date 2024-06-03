0543757fbc93c600c6ac3250d6624d0dff77b6d9
Ihor.Salamin <ihor.salamin.irz.2021@lpnu.ua>
2024-06-03 18:16:47 +0300

    2 commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// build.gradle.kts (Project-level)
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
+
+buildscript {
+    val kotlin_version by extra("1.8.10")
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)
 }
